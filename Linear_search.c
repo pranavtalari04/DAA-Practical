df8f7dceeaf05577dc7777fa9e73325c07593226
pranavtalari04 <pranavtalari04@gmail.com>
2026-08-03 15:53:15 +0530

    Create Linear_search.c

---
+++ Linear_search.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 30, found = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            found = i;
+            break;
+        }
+    }
+
+    if (found != -1)
+        printf("Element found at index %d\n", found);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
